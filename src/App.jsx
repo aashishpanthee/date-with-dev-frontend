ddcc13d180deb088e73603b175831a495c2cba94
Aashish Panthee <aashishpanthee060@gmail.com>
2025-07-06 14:01:52 +0545

    chore: add redux-toolkit & axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Body from './components/Body';
+import Feed from './components/Feed';
+import Login from './components/Login';
 
 function App() {
   return (
     <BrowserRouter basename='/'>
       <Routes>
-        <Route path='/' element={<Body />} />
+        <Route path='/' element={<Body />}>
+          <Route path='/' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
