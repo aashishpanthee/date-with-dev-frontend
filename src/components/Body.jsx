fe45835ed84238fc8e4d816327c6b35dac06128c
Aashish Panthee <aashishpanthee060@gmail.com>
2025-07-06 14:03:04 +0545

    feat: persisting user information throughout the application

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,34 @@
-import { Outlet } from 'react-router';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet, useNavigate } from 'react-router';
+import { addUser } from '../redux/slices/userSlice';
+import { BASE_URL } from '../utils/constants';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (user) return;
+      try {
+        const res = await axios.get(BASE_URL + 'profile/view', { withCredentials: true });
+        dispatch(addUser(res.data.data));
+      } catch (error) {
+        if (error.status === 401) {
+          navigate('/login');
+        }
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, [dispatch, navigate, user]);
   return (
-    <div className='h-full flex items-center justify-center'>
+    <div>
       <Navbar />
       <Outlet />
       <Footer />
