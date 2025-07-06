acaff7c6fb9c148b06bddb8a641183ed26f481ca
Aashish Panthee <aashishpanthee060@gmail.com>
2025-07-06 16:57:31 +0545

    feat: editprofile functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import Body from './components/Body';
 import Feed from './components/Feed';
 import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Body />}>
           <Route path='/' element={<Feed />} />
+          <Route path='/profile' element={<Profile />} />
           <Route path='/login' element={<Login />} />
         </Route>
       </Routes>
---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from 'react-redux';
+import EditProfile from './EditProfile';
+
+const Profile = () => {
+  const user = useSelector((state) => state.user.user);
+  console.log(user);
+  return <div>{user && <EditProfile user={user} />}</div>;
+};
+
+export default Profile;
