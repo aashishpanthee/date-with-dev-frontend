2cb584de20bdd452c7a4c2680069efbc1750084a
Aashish Panthee <aashishpanthee060@gmail.com>
2025-07-07 08:50:38 +0545

    feat: add routes for handling connections & requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Body from './components/Body';
+import Connections from './components/Connections';
 import Feed from './components/Feed';
 import Login from './components/Login';
 import Profile from './components/Profile';
+import Requests from './components/Requests';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path='/' element={<Feed />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/connections' element={<Connections />} />
+          <Route path='/requests' element={<Requests />} />
         </Route>
       </Routes>
     </BrowserRouter>
