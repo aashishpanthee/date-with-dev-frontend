8817fffab6ff690a04d35c7ee3bc5d1816a554a2
Aashish Panthee <aashishpanthee060@gmail.com>
2025-07-07 08:52:08 +0545

    feat: add requests & connections page & functionality

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import connectionSlice from '../slices/connectionSlice';
 import feedSlice from '../slices/feedSlice';
+import requestSlice from '../slices/requestSlice';
 import userSlice from '../slices/userSlice';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     feed: feedSlice,
+    connection: connectionSlice,
+    request: requestSlice
   },
-})
+});
 
 export default store;
